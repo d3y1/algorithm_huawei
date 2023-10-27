1d2941685a20deaab673b1c0865b672253ec7a37
d3y1 <d3y1_2u@163.com>
2023-10-27 12:12:47 +0800

    HJ43 迷宫问题: format code

--- src/main/java/middle/HJ43.java
+++ src/main/java/middle/HJ43.java
@@ -41,11 +41,15 @@ public class HJ43 {
                 }
             }
 
-            solution();
+            solutionDFS();
+//            solutionBFS();
         }
     }
 
-    private static void solution(){
+    /**
+     * solution: DFS
+     */
+    private static void solutionDFS(){
         Step start = new Step(0, 0);
 
         dfs(start);
@@ -68,6 +72,36 @@ public class HJ43 {
             }
         }
         visited[curr.x][curr.y] = false;
+    }
+
+
+    /**
+     * solution: BFS
+     */
+    private static void solutionBFS(){
+        Step start = new Step(0, 0);
+        stepsQueue.add(start);
+
+        bfs();
+
+        printPath(row-1, column-1);
+    }
+
+    private static void bfs(){
+        while (stepsQueue.size()!=0){
+            Step curr = stepsQueue.poll();
+            if(curr.x==row-1 && curr.y==column-1){
+                break;
+            }
+            for(int i=0; i<4; i++){
+                Step next = new Step(curr.x+dX[i], curr.y+dY[i]);
+                if(canGo(next)){
+                    stepsQueue.add(next);
+                    pre[next.x][next.y] = curr;
+                }
+            }
+            visited[curr.x][curr.y] = true;
+        }
     }
 
     private static boolean canGo(Step next){
@@ -110,97 +144,4 @@ public class HJ43 {
             this.y = y;
         }
     }
-
-
-
-//    /**
-//     * bfs
-//     * @param args
-//     */
-//    public static void main(String[] args) {
-//        Scanner in = new Scanner(System.in);
-//
-//        while (in.hasNext()){
-//            row = in.nextInt();
-//            column = in.nextInt();
-//
-//            matrix = new int[row][column];
-//            visited = new boolean[row][column];
-//            pre = new Step[row][column];
-//            for(int i=0; i<row; i++){
-//                for(int j=0; j<column; j++){
-//                    matrix[i][j] = in.nextInt();
-//                }
-//            }
-//
-//            solution();
-//        }
-//    }
-//
-//    private static void solution(){
-//        Step start = new Step(0, 0);
-//        stepsQueue.add(start);
-//
-//        bfs();
-//
-//        printPath(row-1, column-1);
-//    }
-//
-//    private static void bfs(){
-//        while (stepsQueue.size()!=0){
-//            Step curr = stepsQueue.poll();
-//            if(curr.x==row-1 && curr.y==column-1){
-//                break;
-//            }
-//            for(int i=0; i<4; i++){
-//                Step next = new Step(curr.x+dX[i], curr.y+dY[i]);
-//                if(canGo(next)){
-//                    stepsQueue.add(next);
-//                    pre[next.x][next.y] = curr;
-//                }
-//            }
-//            visited[curr.x][curr.y] = true;
-//        }
-//    }
-//
-//    private static boolean canGo(Step next){
-//        // 越界
-//        if(next.x<0 || next.y<0 || next.x>=row || next.y>=column){
-//            return false;
-//        }
-//
-//        // 墙壁
-//        if(matrix[next.x][next.y] == 1){
-//            return false;
-//        }
-//
-//        // 已访问
-//        if(visited[next.x][next.y]){
-//            return false;
-//        }
-//
-//        return true;
-//    }
-//
-//    private static void printPath(int x, int y){
-//        if(x==0 && y==0){
-//            System.out.println("("+x+","+y+")");
-//            return;
-//        }
-//
-//        Step previous = pre[x][y];
-//        printPath(previous.x, previous.y);
-//
-//        System.out.println("("+x+","+y+")");
-//    }
-//
-//    private static class Step {
-//        int x;
-//        int y;
-//
-//        private Step(int x, int y){
-//            this.x = x;
-//            this.y = y;
-//        }
-//    }
 }
