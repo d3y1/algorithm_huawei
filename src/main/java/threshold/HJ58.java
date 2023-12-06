7585c0ed472d32379fd42af934678e457d1a3c7d
d3y1 <d3y1_2u@163.com>
2023-12-06 19:49:59 +0800

    HJ58 输入n个整数，输出其中最小的k个: 最小堆 or 数组

---
+++ src/main/java/threshold/HJ58.java
@@ -0,0 +1,55 @@
+package threshold;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+/**
+ * HJ58 输入n个整数，输出其中最小的k个
+ * @author d3y1
+ */
+public class HJ58{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        while(in.hasNext()){
+            solution1(in);
+            solution2(in);
+        }
+    }
+
+    /**
+     * 最小堆 PriorityQueue
+     * @param in
+     */
+    private static void solution1(Scanner in){
+        int n = in.nextInt();
+        int k = in.nextInt();
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for(int i=1; i<=n; i++){
+            queue.add(in.nextInt());
+        }
+
+        for(int i=1; i<=k; i++){
+            System.out.print(queue.poll()+" ");
+        }
+    }
+
+
+    /**
+     * 数组
+     * @param in
+     */
+    private static void solution2(Scanner in){
+        int n = in.nextInt();
+        int k = in.nextInt();
+
+        int[] nums = new int[n];
+        Arrays.sort(nums);
+
+        for(int i=0; i<k; i++){
+            System.out.print(nums[i]+" ");
+        }
+    }
+}
